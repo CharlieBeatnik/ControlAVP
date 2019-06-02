c6f9e305b97b918a789bca992493ef40f0885835
Andrew Dennison <andy@denno.co.uk>
2019-06-02 23:16:14 +0100

    Added code and tests for new JSON RPC control of Sony TV. Volume control is done.

---
+++ ControllableDevice/JsonRpcDevice.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ControllableDevice
+{
+    class JsonRpcDevice : IDisposable
+    {
+        private bool _disposed = false;
+        private IPAddress _host;
+        private string _preSharedKey;
+        private WebClient _webClient;
+
+        public JsonRpcDevice(IPAddress host, string preSharedKey)
+        {
+            _host = host;
+            _preSharedKey = preSharedKey;
+            _webClient = new WebClient();
+            _webClient.Headers.Add("X-Auth-PSK", _preSharedKey);
+        }
+
+        public JObject Post(JObject json, string path)
+        {
+            string address = $@"http://{_host.ToString()}/{path}";
+            string data = json.ToString(Formatting.None);
+            var response = _webClient.UploadString(address, "POST", data);
+            return JObject.Parse(response);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+            }
+
+            _disposed = true;
+        }
+    }
+}
