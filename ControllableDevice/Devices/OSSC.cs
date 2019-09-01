69f7e1bc3e63d4e34eccaa52e83fc9d2655a0d08
Andrew Dennison <andy@denno.co.uk>
2019-09-01 22:21:18 +0100

    Added basics for OSSC control, although serial isn't sending back the response string quickly enough.

---
+++ ControllableDevice/Devices/OSSC.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllableDevice
+{
+    public class OSSC : IControllableDevice
+    {
+        private bool _disposed = false;
+        private Rs232Device _rs232Device;
+
+        public OSSC(string portId)
+        {
+            _rs232Device = new Rs232Device(portId);
+            _rs232Device.BaudRate = 115200;
+            _rs232Device.MessageTerminator = "\r";
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                _rs232Device.Dispose();
+            }
+
+            _disposed = true;
+        }
+
+        public bool GetAvailable()
+        {
+            return true;
+        }
+
+        public bool SendCommand(uint command)
+        {
+            string commandHex = command.ToString("X");
+            string result = _rs232Device.WriteWithResponse($"send nec 0x{commandHex}", ".*");
+
+            return result != null;
+        }
+    }
+}
---
+++ Tests/ControlRelayTests/TestOSSC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControllableDevice;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestOSSC
+    {
+        private static readonly string _settingsFile = "settings.json";
+        private static JToken _deviceSettings;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext tc)
+        {
+            JObject jsonParsed;
+            using (StreamReader r = new StreamReader(_settingsFile))
+            {
+                string json = r.ReadToEnd();
+                jsonParsed = JObject.Parse(json);
+            }
+
+            _deviceSettings = jsonParsed["OSSC"];
+        }
+        
+        public OSSC CreateDevice()
+        {
+            return new OSSC(_deviceSettings["PortId"].ToString());
+        }
+
+        [TestMethod]
+        public void GivenDevice_WhenSendCommand_ThenResponseIsOK()
+        {
+            using (var device = CreateDevice())
+            {
+                var result = device.SendCommand(0x3EC14DB2);
+                Assert.IsTrue(result);
+            }
+        }
+    }
+}
