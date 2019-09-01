8a6adc4581e699ced5c8fd3173f4220de0097943
Andrew Dennison <andy@denno.co.uk>
2019-09-01 22:05:43 +0100

    Serial Blaster now requires new line to be sent to process a message. It also has a more scalable set of command (e.g. encoding type). Error messages now more complete.

--- SerialBlaster/SerialBlaster.ino
+++ SerialBlaster/SerialBlaster.ino
@@ -1,14 +1,13 @@
 #include <M5StickC.h>
 #include <IRremote.h>
 
-//Examples
-//s0x3EC14DB2
-
 IRsend irsend(M5_IR);
 
 void setup()
 {
   Serial.begin(115200);
+  Serial.setTimeout(60 * 1000);
+
   M5.begin();
   M5.Lcd.println("SerialBlaster");
 
@@ -24,25 +23,60 @@ void loop()
   String in;
   while(Serial.available())
   {
-    in = Serial.readString(); // read the incoming data as string
-    const char* pos = in.c_str();
-    M5.Lcd.println(in.c_str());
+    in = Serial.readStringUntil('\r');
     
-    if(*pos == 's') //send
+    //Example
+    //send nec 0x3EC14DB2
+
+    char *action = strtok((char*)in.c_str(), " ");
+    if (action != NULL)
     {
-      pos++;
-      unsigned long command;
-      strtoul(pos, NULL, 16);
-      irsend.sendNEC(command, 32);
-      Serial.println("OK");
-     
-      digitalWrite(M5_LED, LOW);
-      delay(1000);
-      digitalWrite(M5_LED, HIGH);
+      M5.Lcd.printf("A = %s\n", action);
+      if(strcasecmp(action, "send") == 0)
+      {
+        char *encoding = strtok(NULL, " ");
+        if(encoding != NULL)
+        {
+          M5.Lcd.printf("E = %s\n", encoding);
+          if(strcasecmp(encoding, "nec") == 0)
+          {
+            char *command = strtok(NULL, " ");
+            if(command != NULL)
+            {
+              M5.Lcd.printf("C = %s\n", command);
+              unsigned long commandUL;
+              commandUL = strtoul(command, NULL, 16);
+              irsend.sendNEC(commandUL, 32);
+              Serial.println("OK");
+
+              digitalWrite(M5_LED, LOW);
+              delay(50);
+              digitalWrite(M5_LED, HIGH);
+            }
+            else
+            {
+              Serial.println("ERROR: No command specified.");
+            }
+          }
+          else
+          {
+            Serial.printf("ERROR: Unknown encoding '%s'.\r\n", encoding);
+          }
+        }
+        else
+        {
+          Serial.println("ERROR: No encoding specified.");
+        }
+      }
+      else
+      {
+        Serial.printf("ERROR: Unknown action '%s'.\r\n", action);
+      }
     }
     else
     {
-      Serial.println("ERROR: Unknown message type.");
+      Serial.println("ERROR: Empty message.");
     }
+
   }
 }
