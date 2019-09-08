db1688091ed22e6c6de4314673ee5d762d130ee5
Andrew Dennison <andy@denno.co.uk>
2019-09-08 09:54:51 +0100

    Added OSSC support to ControlAVP cloud interface. All tests pass on both the webapp and relay.

---
+++ AVPCloudToDevice/OSSC.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.Devices;
+using Newtonsoft.Json;
+using ControllableDeviceTypes.OSSCTypes;
+
+
+namespace AVPCloudToDevice
+{
+    public class OSSC
+    {
+        private ServiceClient _serviceClient;
+        private string _deviceId;
+
+        public OSSC(ServiceClient serviceClient, string deviceId)
+        {
+            _serviceClient = serviceClient;
+            _deviceId = deviceId;
+        }
+
+        public bool SendCommand(CommandName commandName)
+        {
+            try
+            {
+                var payload = new { commandName };
+                var response = Utilities.InvokeMethod(_serviceClient, _deviceId, "OSSCSendCommand", payload);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Tests/AVPCloudToDeviceTests/TestOSSC.cs
@@ -0,0 +1,44 @@
+﻿using AVPCloudToDevice;
+using Microsoft.Azure.Devices;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using NUnit.Framework;
+using System.Dynamic;
+using System.IO;
+using ControllableDeviceTypes.OSSCTypes;
+
+namespace Tests
+{
+    class TestOSSC
+    {
+        private dynamic _settings;
+        private readonly string _settingsFile = "settings.json";
+
+        private ServiceClient _serviceClient;
+        private OSSC _device;
+
+        public TestOSSC()
+        {
+            using (StreamReader r = new StreamReader(_settingsFile))
+            {
+                string json = r.ReadToEnd();
+                dynamic parsed = JsonConvert.DeserializeObject<ExpandoObject>(json, new ExpandoObjectConverter());
+                _settings = parsed.OSSC;
+            }
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _serviceClient = ServiceClient.CreateFromConnectionString(_settings.ConnectionString);
+            _device = new OSSC(_serviceClient, _settings.DeviceId);
+        }
+
+        [Test]
+        public void GivenDevice_WhenSendCommand_ThenSuccessIsTrue()
+        {
+            Assert.IsTrue(_device.SendCommand(CommandName.Menu));
+        }
+
+    }
+}
