99e140099ddd524787a8a5897e26a1a11efcdc49
Andrew Dennison <andy@denno.co.uk>
2019-09-08 22:04:20 +0100

    Improved OSSC control page.

--- ControlAVP/ViewComponents/OSSCButtonViewComponent.cs
+++ ControlAVP/ViewComponents/OSSCButtonViewComponent.cs
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
 
 namespace ControlAVP.ViewComponents
 {
-    public class OSSCButtonViewComponent : ViewComponent
+    public class OsscButtonViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync(CommandName commandName)
+        public class Parameters
+        {
+            public CommandName CommandName { get; set; }
+            public string ButtonLabel { get; set; }
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(CommandName commandName, string buttonLabel)
         {
             await Task.FromResult(0); //dummy call to make async
-            return View(commandName);
+            return View(new Parameters(){ CommandName = commandName, ButtonLabel = buttonLabel });
         }
     }
 }
