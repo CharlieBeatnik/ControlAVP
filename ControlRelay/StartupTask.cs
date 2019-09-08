ab3017d79435da461c4e79b4a7bd02ac3b9be14b
Andrew Dennison <andy@denno.co.uk>
2019-09-08 13:12:17 +0100

    Fixed parameter passing to SendCommand function.

--- ControlRelay/StartupTask.cs
+++ ControlRelay/StartupTask.cs
@@ -24,15 +24,15 @@ namespace ControlRelay
 
         public StartupTask()
         {
-            Windows.Storage.StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
-            var config = new LoggingConfiguration();
-            var logfile = new FileTarget("logfile")
-            {
-                    FileName = Path.Combine(localFolder.Path, "log.txt"),
-                    Layout = "${longdate}|${level:uppercase=true}|${callsite:className=True:fileName=False:includeSourcePath=False:methodName=True}|${message} ${exception:format=tostring}"
-            };
-            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
-            LogManager.Configuration = config;
+            //Windows.Storage.StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            //var config = new LoggingConfiguration();
+            //var logfile = new FileTarget("logfile")
+            //{
+            //        FileName = Path.Combine(localFolder.Path, "log.txt"),
+            //        Layout = "${longdate}|${level:uppercase=true}|${callsite:className=True:fileName=False:includeSourcePath=False:methodName=True}|${message} ${exception:format=tostring}"
+            //};
+            //config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+            //LogManager.Configuration = config;
             _logger = LogManager.GetCurrentClassLogger();
         }
 
