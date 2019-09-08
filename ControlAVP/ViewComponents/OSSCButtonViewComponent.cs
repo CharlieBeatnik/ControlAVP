1e325c5088b9871939d4bdd6c1a1123f49f60c50
Andrew Dennison <andy@denno.co.uk>
2019-09-08 18:25:13 +0100

    Made the button on the OSSC page a view component. Later on this will reduce the amount of code duplication and was a good learning experience.

---
+++ ControlAVP/ViewComponents/OSSCButtonViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using ControllableDeviceTypes.OSSCTypes;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlAVP.ViewComponents
+{
+    public class OSSCButtonViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(CommandName commandName)
+        {
+            await Task.FromResult(0); //dummy call to make async
+            return View(commandName);
+        }
+    }
+}
