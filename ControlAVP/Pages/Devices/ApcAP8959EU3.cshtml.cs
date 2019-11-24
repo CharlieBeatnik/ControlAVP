51a13cd6fd899c20d00e702543cff5d712b1b6b9
Andrew Dennison <andy@denno.co.uk>
2019-11-24 12:09:03 +0000

    Moved PDU control to its own page, also made large and small icons for each console.

---
+++ ControlAVP/Pages/Devices/ApcAP8959EU3.cshtml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices;
+using Microsoft.Extensions.Configuration;
+using ControllableDeviceTypes.ApcAP8959EU3Types;
+using Microsoft.AspNetCore.Hosting;
+using AVPCloudToDevice;
+
+namespace ControlAVP.Pages.Devices
+{
+    public class OutletTableViewModel
+    {
+        public IEnumerable<Outlet> Outlets { get; set; }
+        public string WebRootPath { get; set; }
+        public IEnumerable<string> OutletConfirmation { get; set; }
+    }
+
+    public class ApcAP8959EU3Model : PageModel
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _environment;
+
+        private string _connectionString;
+        private string _deviceId;
+        private ServiceClient _serviceClient;
+        private ApcAP8959EU3 _pdu;
+
+        public ApcAP8959EU3Model(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            _configuration = configuration;
+            _environment = environment;
+
+            _connectionString = _configuration.GetValue<string>("ControlAVPIoTHubConnectionString");
+            _deviceId = _configuration.GetValue<string>("ControlAVPIoTHubDeviceId");
+
+            _serviceClient = ServiceClient.CreateFromConnectionString(_connectionString);
+            _pdu = new ApcAP8959EU3(_serviceClient, _deviceId);
+        }
+
+        public static void OnGet()
+        {
+
+        }
+
+        public ActionResult OnGetOutletTable()
+        {
+            var outletTableViewModel = new OutletTableViewModel();
+            outletTableViewModel.Outlets = _pdu.GetOutlets();
+            outletTableViewModel.WebRootPath = _environment.WebRootPath;
+            outletTableViewModel.OutletConfirmation = _configuration.GetSection("OutletConfirmation").Get<string[]>();
+
+            var viewData = new ViewDataDictionary(new Microsoft.AspNetCore.Mvc.ModelBinding.EmptyModelMetadataProvider(), new Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary()) { { "OutletTableViewModel", outletTableViewModel } };
+            viewData.Model = outletTableViewModel;
+
+            PartialViewResult result = new PartialViewResult()
+            {
+                ViewName = "_OutletTable",
+                ViewData = viewData,
+            };
+
+            return result;
+        }
+
+        public ActionResult OnGetToggleOutletState(int id, Outlet.PowerState currentPowerState)
+        {
+            switch (currentPowerState)
+            {
+                case Outlet.PowerState.On:
+                    _pdu.TurnOutletOff(id);
+                    break;
+                case Outlet.PowerState.Off:
+                    _pdu.TurnOutletOn(id);
+                    break;
+            }
+
+            var outletTableViewModel = new OutletTableViewModel();
+            outletTableViewModel.Outlets = _pdu.GetOutletsWaitForPending();
+            outletTableViewModel.WebRootPath = _environment.WebRootPath;
+            outletTableViewModel.OutletConfirmation = _configuration.GetSection("OutletConfirmation").Get<string[]>();
+
+            var viewData = new ViewDataDictionary(new Microsoft.AspNetCore.Mvc.ModelBinding.EmptyModelMetadataProvider(), new Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary()) { { "OutletTableViewModel", outletTableViewModel } };
+            viewData.Model = outletTableViewModel;
+
+            PartialViewResult result = new PartialViewResult()
+            {
+                ViewName = "_OutletTable",
+                ViewData = viewData,
+            };
+
+            return result;
+        }
+
+    }
+}
--- ControlAVP/Pages/Index.cshtml.cs
+++ ControlAVP/Pages/Index.cshtml.cs
@@ -1,100 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Azure.Devices;
 using Microsoft.Extensions.Configuration;
-using ControllableDeviceTypes.ApcAP8959EU3Types;
 using Microsoft.AspNetCore.Hosting;
 using AVPCloudToDevice;
 
 namespace ControlAVP.Pages
 {
-    public class OutletTableViewModel
-    {
-        public IEnumerable<Outlet> Outlets { get; set; }
-        public string WebRootPath { get; set; }
-        public IEnumerable<string> OutletConfirmation { get; set; }
-    }
-
     public class IndexModel : PageModel
     {
-        private readonly IConfiguration _configuration;
-        private readonly IWebHostEnvironment _environment;
-
-        private string _connectionString;
-        private string _deviceId;
-        private ServiceClient _serviceClient;
-        private ApcAP8959EU3 _pdu;
-
-        public IndexModel(IConfiguration configuration, IWebHostEnvironment environment)
+        public IndexModel()
         {
-            _configuration = configuration;
-            _environment = environment;
-
-            _connectionString = _configuration.GetValue<string>("ControlAVPIoTHubConnectionString");
-            _deviceId = _configuration.GetValue<string>("ControlAVPIoTHubDeviceId");
-
-            _serviceClient = ServiceClient.CreateFromConnectionString(_connectionString);
-            _pdu = new ApcAP8959EU3(_serviceClient, _deviceId);
         }
 
         public static void OnGet()
         {
 
         }
-
-        public ActionResult OnGetOutletTable()
-        {
-            var outletTableViewModel = new OutletTableViewModel();
-            outletTableViewModel.Outlets = _pdu.GetOutlets();
-            outletTableViewModel.WebRootPath = _environment.WebRootPath;
-            outletTableViewModel.OutletConfirmation = _configuration.GetSection("OutletConfirmation").Get<string[]>();
-
-            var viewData = new ViewDataDictionary(new Microsoft.AspNetCore.Mvc.ModelBinding.EmptyModelMetadataProvider(), new Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary()) { { "OutletTableViewModel", outletTableViewModel } };
-            viewData.Model = outletTableViewModel;
-
-            PartialViewResult result = new PartialViewResult()
-            {
-                ViewName = "_OutletTable",
-                ViewData = viewData,
-            };
-
-            return result;
-        }
-
-        public ActionResult OnGetToggleOutletState(int id, Outlet.PowerState currentPowerState)
-        {
-            switch (currentPowerState)
-            {
-                case Outlet.PowerState.On:
-                    _pdu.TurnOutletOff(id);
-                    break;
-                case Outlet.PowerState.Off:
-                    _pdu.TurnOutletOn(id);
-                    break;
-            }
-
-            var outletTableViewModel = new OutletTableViewModel();
-            outletTableViewModel.Outlets = _pdu.GetOutletsWaitForPending();
-            outletTableViewModel.WebRootPath = _environment.WebRootPath;
-            outletTableViewModel.OutletConfirmation = _configuration.GetSection("OutletConfirmation").Get<string[]>();
-
-            var viewData = new ViewDataDictionary(new Microsoft.AspNetCore.Mvc.ModelBinding.EmptyModelMetadataProvider(), new Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary()) { { "OutletTableViewModel", outletTableViewModel } };
-            viewData.Model = outletTableViewModel;
-
-            PartialViewResult result = new PartialViewResult()
-            {
-                ViewName = "_OutletTable",
-                ViewData = viewData,
-            };
-
-            return result;
-        }
-
     }
 }
