264de7c11b691fe91b71b92d169436c91e7360fa
Andrew Dennison <andy@denno.co.uk>
2022-07-03 10:57:32 +0100

    Added retry to JsonRpc Post function (as 404 has been seen when changing HDMI inputs).

--- ControllableDevice/JsonRpcDevice.cs
+++ ControllableDevice/JsonRpcDevice.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using NLog;
+using Polly;
 
 namespace ControllableDevice
 {
@@ -17,6 +18,9 @@ namespace ControllableDevice
         private IPAddress _host;
         private string _preSharedKey;
         private WebClientEx _webClient;
+
+        private static readonly int _jsonPostRetryCount = 3;
+        private static readonly TimeSpan _jsonPostWaitBeforeRetry = TimeSpan.FromSeconds(1);
 
         private static Logger _logger = LogManager.GetCurrentClassLogger();
 
@@ -37,23 +41,32 @@ namespace ControllableDevice
                 string data = json.ToString(Formatting.None);
                 string response = string.Empty;
 
+                var policy = Policy<JObject>
+                    .Handle<WebException>()
+                    .WaitAndRetry(retryCount: _jsonPostRetryCount,
+                                  sleepDurationProvider: _ => _jsonPostWaitBeforeRetry,
+                                  onRetry: (exception, sleepDuration, attemptNumber, context) =>
+                                  {
+                                      _logger.Debug($"attemptNumber: {attemptNumber}");
+                                      _logger.Debug($"address: {address}");
+                                      _logger.Debug($"data: {data}");
+                                      _logger.Debug($"response: {response}");
+                                      _logger.Debug(exception.ToString());
+                                  });
+
                 try
                 {
-                    response = _webClient.UploadString(address, "POST", data);
-                    return JObject.Parse(response);
+                    return policy.Execute(() =>
+                    {
+                        response = _webClient.UploadString(address, "POST", data);
+                        return JObject.Parse(response);
+                    });
                 }
-                catch (WebException webException)
+                catch(WebException)
                 {
-                    _logger.Error($"address: {address}");
-                    _logger.Error($"data: {data}");
-                    _logger.Error($"response: {response}");
-                    _logger.Error(webException.ToString());
-                    _logger.Error(webException.ToString());
-                    //The URI formed by combining BaseAddress and address is invalid.
-                    // or
-                    //There was no response from the server hosting the resource.
                     return null;
                 }
+
             }
         }
 
