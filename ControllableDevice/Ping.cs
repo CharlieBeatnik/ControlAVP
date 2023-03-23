61abc8350b0960ddcc718553aaab9515bf83685d
Andrew Dennison <andy@denno.co.uk>
2023-03-23 20:57:45 +0000

    Added a ping implementation, but left on a branch because it doesn't seem possible with UWP.

---
+++ ControllableDevice/Ping.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllableDevice
+{
+    public static class Ping
+    {
+        private static string _executablePath = @"C:\windows\system32\PING.EXE";
+
+        public static bool Send(IPAddress address)
+        {
+            var process = new Process();
+            process.StartInfo.FileName = _executablePath;
+            process.StartInfo.Arguments = address.ToString();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+
+            StreamReader reader = process.StandardOutput;
+            string output = reader.ReadToEnd();
+
+            process.WaitForExit();
+
+            if (process.ExitCode == 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Tests/ControlRelayTests/TestPing.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControllableDevice;
+using System.Net;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestPing
+    {
+        [TestMethod]
+        public void GivenUnavailableIP_WhenSendPing_ThenResponseIsFalse()
+        {
+            var result = Ping.Send(IPAddress.Parse("192.0.2.0"));
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void GivenAvailableIP_WhenSendPing_ThenResponseIsTrue()
+        {
+            var result = Ping.Send(IPAddress.Parse("192.168.0.1"));
+            Assert.IsTrue(result);
+        }
+
+    }
+}
