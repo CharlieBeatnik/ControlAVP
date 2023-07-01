7b5470c8ae97a43ecf2d2d72efcae4babf2645fc
Andrew Dennison <andy@denno.co.uk>
2023-07-01 20:38:41 +0100

    SerialBlaster page now blasts Nec hex codes.

--- ControlAVP/Pages/Devices/SerialBlaster.cshtml.cs
+++ ControlAVP/Pages/Devices/SerialBlaster.cshtml.cs
@@ -1,18 +1,42 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.Azure.Devices;
-using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using AVPCloudToDevice;
+using ControllableDeviceTypes.SerialBlasterTypes;
+using System;
 
 namespace ControlAVP.Pages.Devices
 {
     public class SerialBlasterModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _environment;
+
+        private string _connectionString;
+        private string _deviceId;
+        private ServiceClient _serviceClient;
+        private SerialBlaster _device;
+
+        uint _serialBlasterDeviceIndex;
+
+        public SerialBlasterModel(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            _serialBlasterDeviceIndex = 0;
+
+            _configuration = configuration;
+            _environment = environment;
+
+            _connectionString = _configuration.GetValue<string>("ControlAVPIoTHubConnectionString");
+            _deviceId = _configuration.GetValue<string>("ControlAVPIoTHubDeviceId");
+
+            _serviceClient = ServiceClient.CreateFromConnectionString(_connectionString);
+            _device = new SerialBlaster(_serviceClient, _deviceId, _serialBlasterDeviceIndex);
+        }
+
         public void OnGet()
         {
+
         }
 
         public void OnPostConvertRawHexToNecProtocol(string rawHex, string necHex)
@@ -23,6 +47,9 @@ namespace ControlAVP.Pages.Devices
 
         public void OnPostBlastNecHex(string necHex, string rawHex)
         {
+            uint command = Convert.ToUInt32(necHex, 16);
+            _device.SendCommand(Protocol.Nec, command, 0);
+
             @ViewData["rawHex"] = $"{rawHex}";
             @ViewData["necHex"] = $"{necHex}";
         }
